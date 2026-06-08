fed33abc3cd18ffc9855638b9b8e5778d64c77d9
ibnurifai-hub <111202415889@mhs.dinus.ac.id>
2026-06-09 00:43:12 +0700

    capstone8-CRUD

--- app/Controllers/ProdukController.php
+++ app/Controllers/ProdukController.php
@@ -5,10 +5,44 @@ namespace App\Controllers;
 use App\Controllers\BaseController;
 use CodeIgniter\HTTP\ResponseInterface;
 
+use App\Models\ProductModel;
+
 class ProdukController extends BaseController
 {
+    protected $productModel; 
+
+    function __construct()
+    {
+        helper('form');
+        $this->productModel = new ProductModel();
+    }
+
     public function index()
     {
-        return view('v_produk');
+        return view('produk/index', [
+            'products' => $this->productModel->findAll()
+        ]);
     }
-}
+
+    public function create()
+{
+    $dataFoto = $this->request->getFile('foto');
+
+    $dataForm = [
+        'nama' => $this->request->getPost('nama'),
+        'harga' => $this->request->getPost('harga'),
+        'jumlah' => $this->request->getPost('jumlah') 
+    ];
+
+    if ($dataFoto->isValid()) {
+        $fileName = $dataFoto->getRandomName(); 
+        $dataFoto->move('img/', $fileName);
+        
+        $dataForm['foto'] = $fileName;
+    }
+
+    $this->productModel->insert($dataForm);
+
+    return redirect('produk')->with('success', 'Data Berhasil Ditambah');
+} 
+  }
--- app/Views/v_produk.php
+++
@@ -1,4 +0,0 @@
-<?= $this->extend('layout') ?>
-<?= $this->section('content') ?>
-ini halaman Produk
-<?= $this->endSection() ?>
