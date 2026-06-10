45591faab8beb3b627c2db09db99a0829bfd1e3b
ibnurifai-hub <111202415889@mhs.dinus.ac.id>
2026-06-10 12:36:26 +0700

    edit file readme

--- app/Views/v_keranjang.php
+++ app/Views/v_keranjang.php
@@ -1,4 +1,54 @@
 <?= $this->extend('layout') ?>
 <?= $this->section('content') ?>
-ini halaman keranjang
+<?php
+if (session()->getFlashData('success')) {
+?>
+    <div class="alert alert-success alert-dismissible fade show" role="alert">
+        <?= session()->getFlashData('success') ?>
+        <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+    </div>
+<?php
+}
+?> 
+<form action="<?= base_url('keranjang/edit') ?>" method="post">
+<!-- Table with stripped rows -->
+<table class="table datatable">
+    <thead>
+        <tr>
+            <th scope="col">Nama</th>
+            <th scope="col">Foto</th>
+            <th scope="col">Harga</th>
+            <th scope="col">Jumlah</th>
+            <th scope="col">Subtotal</th>
+            <th scope="col">Aksi</th>
+        </tr>
+    </thead>
+    <tbody>
+        <?php
+        $i = 1;
+        if (!empty($items)) :
+            foreach ($items as $index => $item) :
+        ?>
+                <tr>
+                    <td><?php echo $item['name'] ?></td>
+                    <td><img src="<?php echo base_url() . "img/" . $item['options']['foto'] ?>" width="100px"></td>
+                    <td>Rp <?= number_format($item['price'], 0, ',', '.') ?></td> 
+                    <td><input type="number" min="1" name="qty<?= $i++ ?>" class="form-control" value="<?= $item['qty'] ?>"></td>
+                    <td>Rp <?= number_format($item['subtotal'], 0, ',', '.') ?></td>
+                    <td>
+                    <a href="<?= base_url('keranjang/delete/' . $item['rowid'] . '') ?>" class="btn btn-danger"><i class="bi bi-trash"></i></a>
+                    </td>
+                </tr>
+        <?php
+            endforeach;
+        endif;
+        ?>
+    </tbody>
+</table>
+<div class="alert alert-info">
+    Total = Rp <?= number_format($total, 0, ',', '.') ?>
+</div>
+<button type="submit" class="btn btn-primary">Perbarui Keranjang</button>
+<a class="btn btn-warning" href="<?= base_url() ?>keranjang/clear">Kosongkan Keranjang</a>
+</form>
 <?= $this->endSection() ?>
